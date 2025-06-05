e4cc2ed580afd2593bc812b159f92b0b46ec30eb
Carlosvleon <vallejos.leon.carlos@gmail.com>
2025-06-05 19:52:04 -0400

    commit provicsinal

--- backend-ferreteria/src/app.js
+++ backend-ferreteria/src/app.js
@@ -18,7 +18,7 @@ const perfilRoutes = require("./routes/perfilRoutes");
 const oficioRoutes = require("./routes/oficioRoutes");
 const productoRoutes = require("./routes/productoRoutes");
 const bodegaRoutes = require("./routes/bodegaRoutes");
-const carritoRoutes = require("./routes/carritoRoutes");
+const carritoRoutes = require('./routes/carritoRoutes');
 
 app.use("/api/test", testRoutes);
 app.use("/api/usuarios", userRoutes); // login, register, cerrar sesión
@@ -26,6 +26,6 @@ app.use("/api/perfiles", perfilRoutes); // perfil_user, perfiles_users, perfil_b
 app.use("/api/oficios", oficioRoutes); // llamar_oficios, agregar_oficios, actualizar_estado_oficio
 app.use("/api/productos", productoRoutes); // producto, bodega_producto
 app.use("/api/bodegas", bodegaRoutes); // bodega_sucursal
-app.use("/api/carrito", carritoRoutes); // carrito_compras
+app.use('/api/carrito', carritoRoutes); // carrito_compras
 
 module.exports = app;
--- backend-ferreteria/src/models/ProductoModel.js
+++ backend-ferreteria/src/models/ProductoModel.js
@@ -1,7 +1,9 @@
 const pool = require('../db');
 
 exports.obtenerTodos = async () => {
-  const result = await pool.query("SELECT * FROM producto ORDER BY id_producto ASC");
+  const result = await pool.query(
+    "SELECT * FROM producto p LEFT JOIN precios_online po ON po.id_producto = p.id_producto  ORDER BY p.id_producto ASC"
+  );
   return result.rows;
 };
 
@@ -16,15 +18,17 @@ exports.obtenerPorBodega = async (id_bodega, id_sucursal) => {
        s.nombre AS "Sucursal",
        b.nombre AS "Bodega",
        bp.fecha_modificacion AS "Fecha",
-       bp.ultimo_precio AS "Precio"
+       bp.ultimo_precio AS "Precio",
+       po.precio AS "Precio Online"
      FROM bodega_producto bp
      JOIN producto p ON bp.id_producto = p.id_producto
      JOIN bodega b ON bp.id_bodega = b.id_bodega
      JOIN sucursal s ON b.id_sucursal = s.id_sucursal
      LEFT JOIN marca ma ON ma.id_marca = p.id_marca
      LEFT JOIN modelo mo ON mo.id_modelo = p.id_modelo
+     LEFT JOIN precios_online po on po.id_producto = p.id_producto
      WHERE b.id_bodega = $1 AND s.id_sucursal = $2
-     ORDER BY p.id_producto ASC`,
+     ORDER BY p.id_producto ASC`,     
     [id_bodega, id_sucursal]
   );
   return result.rows;
