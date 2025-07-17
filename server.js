96580e504db3b63ed8c5dc13be21a81dfb41bd8e
Carlosvleon <vallejos.leon.carlos@gmail.com>
2025-07-16 22:34:34 -0400

    another test for deply

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
+if (!port) throw new Error('PORT environment variable not set');
 
 app.use(express.static(path.join(__dirname, 'dist/frontend-ferreteria/browser')));
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/frontend-ferreteria/browser/index.html'));
 });
-
 app.listen(port, () => {
   console.log(`Servidor iniciado en http://localhost:${port}`);
 });
