f621fb0df682e91ea1d276ffa9948350e4012f6e
Carlosvleon <vallejos.leon.carlos@gmail.com>
2025-07-16 22:49:08 -0400

    otro intento

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT; // NO uses fallback a 8080
+
 if (!port) throw new Error('PORT environment variable not set');
 
 app.use(express.static(path.join(__dirname, 'dist/frontend-ferreteria/browser')));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/frontend-ferreteria/browser/index.html'));
 });
+
 app.listen(port, () => {
   console.log(`Servidor iniciado en http://localhost:${port}`);
 });
