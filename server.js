098c08b61bb0f0c70e972a4e067d568c89f293e1
Carlosvleon <vallejos.leon.carlos@gmail.com>
2025-07-16 22:03:26 -0400

    fix: serve to expres

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-const serve = require('serve');
+const express = require('express');
+const path = require('path');
+const app = express();
 const port = process.env.PORT || 8080;
-serve('dist/frontend-ferreteria/browser', {
-  single: true,
-  port: port
+
+app.use(express.static(path.join(__dirname, 'dist/frontend-ferreteria/browser')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/frontend-ferreteria/browser/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor iniciado en http://localhost:${port}`);
 });
