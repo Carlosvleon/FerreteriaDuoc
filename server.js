abaa7671f38b2c231d739109b3216a79e16979af
Carlosvleon <vallejos.leon.carlos@gmail.com>
2025-07-16 23:02:37 -0400

    asdasdasd

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-
-console.log("=== INICIANDO SERVER.JS DESDE EL CONTENEDOR ===");
-
+// server.js
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT; // NO uses fallback a 8080
+const port = process.env.PORT || 8080;
 
-if (!port) throw new Error('PORT environment variable not set');
+console.log("=== INICIANDO SERVER.JS DESDE EL CONTENEDOR ===");
+app.get('/ping', (req, res) => res.send('pong'));
 
 app.use(express.static(path.join(__dirname, 'dist/frontend-ferreteria/browser')));
 
